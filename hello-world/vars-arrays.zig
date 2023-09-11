2ac1e0649c1977ae4175c6c689ef3617effb800b
Nishant Srinivas <nishantsrinivas@hotmail.com>
2023-09-11 21:35:58 +0530

    practice creating arrays

--- hello-world/vars-arrays.zig
+++ hello-world/vars-arrays.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn main() void {
     std.debug.print("Variables and Arrays!!\n", .{});
 
+    // Variables
     const constant: u32 = 100;
     var variable: u32 = 200;
 
@@ -17,5 +18,13 @@ pub fn main() void {
     // We can't do this 
     // constant -= 50; error: cannot assign to constant
     std.debug.print("constant = {d}\n", .{constant});
+
+    // Arrays
+    const my_array = [10]u8{1,2,3,4,5,6,7,8,9,10}; // Program doesn't run if we have unused vars!!
+
+    std.debug.print("{d}\n", .{my_array[9]});
+
+    const len_my_array = my_array.len;
+    std.debug.print("len of my array is {d}\n", .{len_my_array});
 
 }
