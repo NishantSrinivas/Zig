af0c1b8b9cd204dec135bb815eb6e65312917ebf
Nishant Srinivas <nishantsrinivas@hotmail.com>
2023-09-13 21:12:42 +0530

    add example for defer keyword

--- hello-world/functions.zig
+++ hello-world/functions.zig
@@ -9,9 +9,32 @@ fn fib(x: i32) i32{
 }
 
 pub fn main() void {
-    const res = fib(1000);
+    const res = fib(10);
 
     std.debug.print("res = {d}\n", .{res});
+
+    // Defer -> executes when exiting of code block
+    var num: u8 = 10;
+    {
+        defer num += 10;
+        // Num should be 10 only here
+        std.debug.print("num = {d}\n", .{num});
+    }
+
+    // Num should be 20 here
+    std.debug.print("num after defer = {d}\n", .{num});
+
+    // Defer stacking
+    {
+        defer num *= 3;
+        defer num += 10;
+        // Num should be 20 only here
+        std.debug.print("num = {d}\n", .{num});
+    }
+
+    // Num should be 90 here
+    std.debug.print("num after defer = {d}\n", .{num});
+
 }
 
 // 0 1 1 2 3 5 8
